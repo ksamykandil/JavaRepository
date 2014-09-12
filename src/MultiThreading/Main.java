f6d12874dfcaacaa6bba24d409ba43542db5a029
Khaled Kandil <ksamykandil@gmail.com>
2014-09-12 20:16:13 +0300

    Adjusting to be imported in eclipse smoothly

---
+++ src/MultiThreading/Main.java
@@ -0,0 +1,19 @@
+package MultiThreading;
+/**
+ * Created by ksamykandil on 8/23/14.
+ */
+public class Main {
+    public static void main(String[] args){
+        RunnableDemo r1 = new RunnableDemo("Runnable1");
+        r1.start();
+
+        RunnableDemo r2 = new RunnableDemo("Runnable2");
+        r2.start();
+
+        ThreadDemo t1 = new ThreadDemo("Thread1");
+        t1.start();
+
+        ThreadDemo t2 = new ThreadDemo("Thread2");
+        t2.start();
+    }
+}
